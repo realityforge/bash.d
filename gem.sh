60687ede9e0ff0aa438f545d96ea3ec654187448
Peter Donald <peter@realityforge.org>
2017-07-26 12:10:19 +1000

    Configure gemrc setup

---
+++ gem.sh
@@ -0,0 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ ! -f "${DIR}/cache/gem.1" ]; then
+  rm -f ~/.gemrc
+  echo "install: --no-rdoc --no-ri" > ~/.gemrc
+  echo "update: --no-rdoc --no-ri" >> ~/.gemrc
+	
+  mkdir -p ${DIR}/cache
+  touch ${DIR}/cache/gem.1
+fi
+	
