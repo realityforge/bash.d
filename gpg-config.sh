0f230caf2f98a7e9475fe7a06a74f8fd0340dc71
Peter Donald <peter@realityforge.org>
2017-08-01 22:53:35 +1000

    Make grep quiet

--- gpg-config.sh
+++ gpg-config.sh
@@ -1,3 +1,3 @@
 
 # Ensure no-tty is set so IDEA can sign commits
-grep -v no-tty ~/.gnupg/gpg.conf || echo no-tty >>  ~/.gnupg/gpg.conf
+grep -q no-tty ~/.gnupg/gpg.conf || echo no-tty >>  ~/.gnupg/gpg.conf
