baca005f3b013f8ba98091fb5f98a002bf61c6f3
Peter Donald <peter@realityforge.org>
2018-12-04 17:28:29 +1100

    Some config to make it easier to use d8 for perf analysis

---
+++ d8.sh
@@ -0,0 +1,3 @@
+alias d8=/Users/peter/Code/realityforge/v8/out.gn/x64.optdebug/d8
+alias tick-processor=/Users/peter/Code/realityforge/v8/tools/mac-tick-processor
+export D8_PATH="/Users/peter/Code/realityforge/v8/out.gn/x64.optdebug"
