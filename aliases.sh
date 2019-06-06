a3e5d1cf2232d798e47111d0d35258a52d21998a
Peter Donald <peter@realityforge.org>
2019-06-07 09:23:41 +1000

    Add alias to prerty bazel files

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,5 @@ alias rake="bundle exec rake"
 alias rspec="bundle exe rspec"
 alias braid="bundle exec braid"
 alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`"
+
+alias bzl-pretty="buildifier \$(find . -type f \\( -iname BUILD -or -iname BUILD.bazel  -or -name \\*.bzl -or -iname WORKSPACE \\))"
