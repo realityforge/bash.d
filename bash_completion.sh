57e5b03690de47545dae76e260432c6e36ca8170
Peter Donald <peter@realityforge.org>
2022-03-02 18:38:06 +1100

    Get bash completion from all the places

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,5 +1,16 @@
-_source_files=`ls /usr/local/etc/bash_completion.d/*.sh`
-for f in $_source_files
-do
-  source $f
-done
+if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
+  . /opt/local/etc/profile.d/bash_completion.sh
+fi
+
+[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+
+if [ -d /usr/local/etc/bash_completion.d ]; then
+  _source_files=`ls /usr/local/etc/bash_completion.d/*.sh`
+  for f in $_source_files
+  do
+    source $f
+  done
+fi
+if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
+    . /opt/local/etc/profile.d/bash_completion.sh
+fi
