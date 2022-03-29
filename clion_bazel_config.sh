f9778e3730e0b9b8932408b695d92b072340f721
Peter Donald <peter@realityforge.org>
2022-03-29 15:40:22 +1100

    Fix CLion/Bazel integration woes

---
+++ clion_bazel_config.sh
@@ -0,0 +1,5 @@
+# Not entirely sure why this is required but CLion starts wiorking if I add these in.
+# See https://github.com/bazelbuild/bazel/issues/10533
+
+export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
+export SDKROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/"
