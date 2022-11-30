e09cb7c3db7ac0b8409ed721ea337aa70dc0ec3f
Peter Donald <peter@realityforge.org>
2022-12-01 10:43:18 +1100

    Set push.autoSetupRemote to true.
    The documentation for this is:
    
           push.autoSetupRemote
               If set to "true" assume --set-upstream on default push when no upstream tracking exists for the current branch; this option takes effect with push.default options
               simple, upstream, and current. It is useful if by default you want new branches to be pushed to the default remote (like the behavior of push.default=current) and you
               also want the upstream tracking to be set. Workflows most likely to benefit from this option are simple central workflows where all branches are expected to have the
               same name on the remote.

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ ! -f "${DIR}/cache/git.4" ]; then
+if [ ! -f "${DIR}/cache/git.6" ]; then
   git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
   git config --global alias.expire-branches '!git branch  --no-color | grep -v \* | grep -v -x "  master" | grep -v -x "  upstream" | xargs git branch -d'
   git config --global alias.expire-branches-force '!git branch --no-color | grep -v \* | grep -v -x "  master" | grep -v -x "  upstream" | xargs git branch -D'
@@ -12,11 +12,12 @@ if [ ! -f "${DIR}/cache/git.4" ]; then
   git config --global gpg.program /usr/local/MacGPG2/bin/gpg
   git config --global gui.gcwarning false
   git config --global push.default simple
+  git config --global push.autoSetupRemote true
 
   git config --global diff.indentheuristic true
 
   git config --global core.excludesfile $DIR/git/gitignore_global
 
   mkdir -p ${DIR}/cache
-  touch ${DIR}/cache/git.4
+  touch ${DIR}/cache/git.6
 fi
